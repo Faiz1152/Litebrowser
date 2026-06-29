71243972d629fa7358908e1782709ca27fb664cc
Faiz1152 <khatrifaiz001@gmail.com>
2026-06-29 12:54:00 +0400

    Delete litebrowser directory

--- litebrowser/src/client_handler.cpp
+++
@@ -1,9 +0,0 @@
-#include "client_handler.h"
-
-void ClientHandler::OnAfterCreated(CefRefPtr<CefBrowser> browser) {
-  browser_ = browser;
-}
-
-void ClientHandler::OnBeforeClose(CefRefPtr<CefBrowser> browser) {
-  browser_ = nullptr;
-}
