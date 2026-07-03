3167df845d151186d0ac9c9c0e980f260b8231b6
Faiz1152 <khatrifaiz001@gmail.com>
2026-07-03 19:39:08 +0400

    Refactor ClientHandler to include additional handlers

--- src/client_handler.h
+++ src/client_handler.h
@@ -1,32 +1,55 @@
-// ClientHandler wires up the browser window lifecycle: knowing when it's
-// ready, when it closes, and keeping a reference to it so main.cpp can
-// resize it and send it new addresses to load.
 #pragma once
-
 #include "include/cef_client.h"
+#include "include/cef_life_span_handler.h"
+#include "include/cef_display_handler.h"
+#include "include/cef_request_handler.h"
+#include "include/cef_resource_request_handler.h"
+#include <string>
+
+extern bool g_blockAds;
+extern bool g_blockTrackers;
+extern bool g_blockPopups;
+extern bool g_focus;
+
+void OnBrowserCreated(int tabId, CefRefPtr<CefBrowser> browser);
+void OnTitleChanged(int tabId, const std::wstring& title);
+void OnUrlChanged(int tabId, const std::wstring& url);
+void AddHistory(const std::wstring& title, const std::wstring& url);
 
 class ClientHandler : public CefClient,
-                       public CefLifeSpanHandler,
-                       public CefDisplayHandler {
- public:
-  ClientHandler() = default;
+                      public CefLifeSpanHandler,
+                      public CefDisplayHandler,
+                      public CefRequestHandler,
+                      public CefResourceRequestHandler {
+public:
+    explicit ClientHandler(int tabId) : tab_id_(tabId) {}
 
-  CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() override { return this; }
-  CefRefPtr<CefDisplayHandler> GetDisplayHandler() override { return this; }
+    CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() override { return this; }
+    CefRefPtr<CefDisplayHandler>  GetDisplayHandler()  override { return this; }
+    CefRefPtr<CefRequestHandler>  GetRequestHandler()  override { return this; }
 
-  // CefLifeSpanHandler
-  void OnAfterCreated(CefRefPtr<CefBrowser> browser) override;
-  void OnBeforeClose(CefRefPtr<CefBrowser> browser) override;
+    void OnAfterCreated(CefRefPtr<CefBrowser> browser) override;
+    void OnBeforeClose(CefRefPtr<CefBrowser> browser) override;
 
-  // CefDisplayHandler - keep window title plain; we don't show a title bar
-  // address, but this is here in case you want it later.
-  void OnTitleChange(CefRefPtr<CefBrowser> browser,
-                      const CefString& title) override {}
+    void OnTitleChange(CefRefPtr<CefBrowser> browser,
+                       const CefString& title) override;
+    void OnAddressChange(CefRefPtr<CefBrowser> browser,
+                         CefRefPtr<CefFrame> frame,
+                         const CefString& url) override;
 
-  CefRefPtr<CefBrowser> GetBrowser() { return browser_; }
+    CefRefPtr<CefResourceRequestHandler> GetResourceRequestHandler(
+        CefRefPtr<CefBrowser>, CefRefPtr<CefFrame>, CefRefPtr<CefRequest>,
+        bool, bool, const CefString&, bool&) override { return this; }
 
- private:
-  CefRefPtr<CefBrowser> browser_;
+    ReturnValue OnBeforeResourceLoad(CefRefPtr<CefBrowser>, CefRefPtr<CefFrame>,
+                                      CefRefPtr<CefRequest>,
+                                      CefRefPtr<CefCallback>) override;
 
-  IMPLEMENT_REFCOUNTING(ClientHandler);
+    CefRefPtr<CefBrowser> GetBrowser() { return browser_; }
+
+private:
+    int tab_id_;
+    CefRefPtr<CefBrowser> browser_;
+    static bool IsBlocked(const std::string& url);
+    IMPLEMENT_REFCOUNTING(ClientHandler);
 };
