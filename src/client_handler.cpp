0c8f5118671c95cc2b5fdb89307a72770df4d057
Faiz1152 <khatrifaiz001@gmail.com>
2026-07-06 14:30:06 +0400

    Add more domains to block list

--- src/client_handler.cpp
+++ src/client_handler.cpp
@@ -1,23 +1,38 @@
 #include "client_handler.h"
 
 static const char* kBlockList[] = {
-    // Ads
+    // Display & programmatic ads
     "doubleclick.net","googlesyndication.com","googleadservices.com",
     "adnxs.com","advertising.com","adroll.com","criteo.com","criteo.net",
     "outbrain.com","taboola.com","pubmatic.com","rubiconproject.com",
     "casalemedia.com","openx.net","moatads.com","amazon-adsystem.com",
     "adsafeprotected.com","media.net","sharethrough.com","bidswitch.net",
+    "adsrvr.org","adform.net","appnexus.com","smartadserver.com",
+    "lijit.com","sovrn.com","33across.com","triplelift.com",
+    "indexexchange.com","yieldmo.com","rhythmone.com","spotxchange.com",
+    "spotx.tv","undertone.com","conversantmedia.com","turn.com",
     // Trackers
     "google-analytics.com","googletagmanager.com","hotjar.com",
     "mixpanel.com","segment.com","segment.io","amplitude.com",
     "fullstory.com","heap.io","chartbeat.com","newrelic.com",
     "connect.facebook.net","bat.bing.com","analytics.twitter.com",
+    "scorecardresearch.com","quantserve.com","comscore.com",
+    "krxd.net","bluekai.com","demdex.net","rlcdn.com",
+    "adtechus.com","adblade.com","mathtag.com","exelator.com",
+    // YouTube ads
+    "googleads.g.doubleclick.net","static.doubleclick.net",
+    "pagead2.googlesyndication.com","ad.youtube.com",
+    "ads.youtube.com","imasdk.googleapis.com",
+    "youtube.com/api/stats/ads","youtube.com/pagead",
+    "youtube.com/ptracking","youtubei.googleapis.com/youtubei/v1/log",
     nullptr
 };
 
 static const char* kFocusBlock[] = {
     "facebook.com","twitter.com","x.com","instagram.com","reddit.com",
     "youtube.com","tiktok.com","snapchat.com","twitch.tv","discord.com",
+    "netflix.com","primevideo.com","disneyplus.com","hulu.com",
+    "9gag.com","pinterest.com","tumblr.com","linkedin.com",
     nullptr
 };
 
